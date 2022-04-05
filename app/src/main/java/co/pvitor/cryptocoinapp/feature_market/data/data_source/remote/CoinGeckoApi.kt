81ed05298e9ccaf97fb96a8a82ced6832644848b
Paulo <pvitor.db@gmail.com>
2022-04-04 21:21:12 -0300

    API - Coingecko

--- app/src/main/java/co/pvitor/cryptocoinapp/feature_market/data/data_source/remote/CoinGeckoApi.kt
+++ app/src/main/java/co/pvitor/cryptocoinapp/feature_market/data/data_source/remote/CoinGeckoApi.kt
@@ -2,16 +2,25 @@ package co.pvitor.cryptocoinapp.feature_market.data.data_source.remote
 
 import co.pvitor.cryptocoinapp.feature_market.data.data_source.remote.dto.CoinDto
 import retrofit2.http.GET
+import retrofit2.http.Header
 import retrofit2.http.Path
+import retrofit2.http.Query
 
 interface CoinGeckoApi {
 
+    companion object {
+        const val BASE_URL = "https://api.coingecko.com/"
+        const val API_VERSION = "/api/v3"
+    }
 
+    @GET("$API_VERSION/coins/markets")
+    suspend fun getMarketCoins(
+        @Query("vs_currency") currency: String? = "usd",
+        @Query("page") page: Int?,
+        @Query("per_page") per_page: Int?
+    ) : List<CoinDto>
 
-    @GET("/coins/markets")
-    suspend fun getMarketCoins() : List<CoinDto>
-
-    @GET("/coins/{id}")
+    @GET("$API_VERSION/coins/{id}")
     suspend fun getCoinById(@Path("id") id: String) : CoinDto
 
 }
